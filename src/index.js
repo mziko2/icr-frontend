88dab08bea2b6d312ddb77fbbb21da5ac63c256e
Mirza Žiko <mziko2@etf.unsa.ba>
2021-08-25 15:43:20 +0200

    nav bar added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './main-page';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarInstance from './main-page/navigation';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <NavBarInstance/>
+     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/main-page/navigation.js
@@ -0,0 +1,24 @@
+import { Nav, Navbar } from 'rsuite';
+
+
+const NavBarInstance = ({ onSelect, activeKey, ...props }) => {
+    return (
+      <Navbar {...props}>
+        <Navbar.Header>
+          AutoŠkola LECON
+        </Navbar.Header>
+        <Navbar.Body>
+          <Nav onSelect={onSelect} activeKey={activeKey}>
+            <Nav.Item eventKey="1">Početna</Nav.Item>
+            <Nav.Item eventKey="2">O nama</Nav.Item>
+            <Nav.Item eventKey="3">Registruj se</Nav.Item>
+            <Nav.Item eventKey="4">Konkursi</Nav.Item>
+            <Nav.Item eventKey="5">FAQ</Nav.Item>
+          </Nav>
+          
+        </Navbar.Body>
+      </Navbar>
+    );
+  };
+
+export default NavBarInstance;
