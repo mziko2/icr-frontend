e29a48a78cb52bc75a98145e0545c931845a6ba6
Emir Kurtovic <ekurtovic3@etf.unsa.ba>
2021-08-25 23:16:50 +0200

    FAQ

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -3,10 +3,11 @@ import lecon from './lecon.png';
 import { Button } from 'rsuite';
 import Pocetna from './pocetna'
 import Onama from './onama'
+import FAQ from './faq'
 function App() {
   return (
     <div className="App">
-    <Onama/>
+    <FAQ/>
        </div>
   );
 }
