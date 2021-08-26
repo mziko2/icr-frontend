b1d946539f9e679ae058e3951772a20ab19625be
Emir Kurtovic <ekurtovic3@etf.unsa.ba>
2021-08-26 22:34:48 +0200

    Login stranica

--- src/main-page/login.js
+++ src/main-page/login.js
@@ -1,14 +1,30 @@
 import './login.css';
 import './Onama.css';
+import { Button } from 'rsuite';
+
 import Footer from '../components/Footer'
 
 function Login() {
     return (
         <div className="login">
-            <div>Prijavi se</div>
-            <div>Prijavi se</div>
+            <div className="naslov">Prijavi se</div>
+            <div className="fields">
+                <div className="field">
+                    <div className="label">Username</div>
+                    <input type="text" /></div>
+                
+                <div className="field">
 
+                    <div className="label">Password
+                    </div><input type="text" /></div></div>
+            <div>
+                <Button className="buttonPrijava" >Prijava</Button>
+            </div>
+            <div className="color">
+                Zaboravili ste username ili password ?</div>
+            <div className="registrujSe">  <div>  Niste registrovani ?  </div><div className="color2">  Registrujte se!</div></div>
         </div>
+
     );
 }
 
